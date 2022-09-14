79b26968720536c0785853683fa47b66d18a75af
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-09-14 21:50:48 +0700

    lab2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_xml 
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_xml, name='show_xml'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_wishlist(request):
@@ -9,3 +11,7 @@ def show_wishlist(request):
         'nama': 'Thalia'
     }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
