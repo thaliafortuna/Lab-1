46c26cadea0553036a1ff7d3aa37fbfaf6370a04
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-09-15 13:25:06 +0700

    show by id

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
-    path('xml/<int:id>', show_json_by_id, name='show_xml_by_id'),
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
     
 ]
