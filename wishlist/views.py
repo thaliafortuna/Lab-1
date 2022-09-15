7d67832a2112c35c9ee6526c5fadfd5a54b8ad89
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-09-15 11:12:46 +0700

    show json by id and show xml by id

--- wishlist/views.py
+++ wishlist/views.py
@@ -15,3 +15,15 @@ def show_wishlist(request):
 def show_xml(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request,id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request,id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
